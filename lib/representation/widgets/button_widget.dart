32f1f834e5c768c2dbe4ce34f9a1c33f66d58ab7
Truong Anim <truong_anim@Truongs-iMac.local>
2023-04-06 02:05:10 +0700

    add page view indicator

---
+++ lib/representation/widgets/button_widget.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:travel_app_ui/core/constants/color_constants.dart';
+import 'package:travel_app_ui/core/constants/dismention_constants.dart';
+import 'package:travel_app_ui/core/constants/textstyle_constants.dart';
+
+class ButtonWidget extends StatelessWidget {
+  const ButtonWidget({super.key, required this.title, this.onTap});
+  final String title;
+  final VoidCallback? onTap;
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        alignment: Alignment.center,
+        padding: const EdgeInsets.symmetric(vertical: kDefaultPadding),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(kMediumPadding),
+            gradient: Gradients.defaultGradient),
+        child: Text(
+          title,
+          style: TextStyles.defaultStyle.whiteTextColor.bold,
+        ),
+      ),
+    );
+  }
+}
--- lib/routes.dart
+++ lib/routes.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:travel_app_ui/representation/screen/intro_screen.dart';
+import 'package:travel_app_ui/representation/screen/main_app_screen.dart';
 import 'representation/screen/splash_screen.dart';
 
 final Map<String, WidgetBuilder> routes = {
   SplashScreen.routerName: (context) => const SplashScreen(),
   IntroScreen.routerName: (context) => const IntroScreen(),
+  MainAppScreen.routeName: (context) => const MainAppScreen(),
 };
