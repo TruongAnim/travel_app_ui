dceb33714425346c07b18941dbbcfe017c2d4975
TruongAnim <truonganim.dev@gmail.com>
2023-04-06 09:40:01 +0700

    add bottom bar

--- lib/representation/screen/main_app_screen.dart
+++ lib/representation/screen/main_app_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:salomon_bottom_bar/salomon_bottom_bar.dart';
+import 'package:travel_app_ui/core/constants/color_constants.dart';
+import 'package:travel_app_ui/core/constants/dismention_constants.dart';
 
 class MainAppScreen extends StatefulWidget {
   const MainAppScreen({super.key});
@@ -9,10 +13,46 @@ class MainAppScreen extends StatefulWidget {
 }
 
 class _MainAppScreenState extends State<MainAppScreen> {
+  int _currentIndex = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(),
+      bottomNavigationBar: SalomonBottomBar(
+          currentIndex: _currentIndex,
+          onTap: (index) {
+            setState(() {
+              _currentIndex = index;
+            });
+          },
+          selectedItemColor: ColorPalette.primaryColor,
+          unselectedItemColor: ColorPalette.primaryColor.withOpacity(0.2),
+          items: [
+            SalomonBottomBarItem(
+                icon: const Icon(
+                  FontAwesomeIcons.house,
+                  size: kDefaultIconSize,
+                ),
+                title: const Text('Home')),
+            SalomonBottomBarItem(
+                icon: const Icon(
+                  FontAwesomeIcons.solidHeart,
+                  size: kDefaultIconSize,
+                ),
+                title: const Text('Likes')),
+            SalomonBottomBarItem(
+                icon: const Icon(
+                  FontAwesomeIcons.briefcase,
+                  size: kDefaultIconSize,
+                ),
+                title: const Text('Booking')),
+            SalomonBottomBarItem(
+                icon: const Icon(
+                  FontAwesomeIcons.solidUser,
+                  size: kDefaultIconSize,
+                ),
+                title: const Text('User'))
+          ]),
     );
   }
 }
